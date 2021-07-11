bb3a4362228059d4619bd9add9b3b0d46abdce9b
Simon Harvan <harvan.simon@gmail.com>
2021-07-11 14:37:39 +0200

    Make additional fields nullable

---
+++ database/migrations/2021_07_11_123611_additional_fields_nullable.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AdditionalFieldsNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+	public function up()
+	{
+		Schema::table('registrations', function (Blueprint $table) {
+			$table->string('additional_field')->nullable()->change();
+			$table->string('additional_field_2')->nullable()->change();
+			$table->string('additional_field_3')->nullable()->change();
+			$table->string('additional_field_4')->nullable()->change();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('event_texts', function (Blueprint $table) {
+			$table->string('additional_field')->change();
+			$table->string('additional_field_2')->change();
+			$table->string('additional_field_3')->change();
+			$table->string('additional_field_4')->change();
+		});
+	}
+}
