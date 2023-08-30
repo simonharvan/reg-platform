166fbb7f02b31af2fe2c7f0a7c8d691ad8f72997
Simon Harvan <harvan.simon@gmail.com>
2023-08-30 10:26:53 +0200

    Add spanish

---
+++ resources/lang/es/app.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'welcome' => 'Welcome!',
+
+    'registerNow' => 'Register Now',
+    'fieldsAreRequired' => 'Campos marcados con (<span class="required">*</span>) son obligatorios',
+    'personal' => 'INFORMACIÓN PERSONAL',
+    'delegation' => 'DELEGATION DATA',
+    'travel' => 'TRAVEL / VISA DATA',
+    'accommodation' => 'HOTEL ACCOMMODATION',
+    'additional' => 'INFORMACIÓN ADICIONAL',
+
+    'registrationSuccess' => 'The registration form was submitted successfully',
+    'thanks' => 'THANK YOU!',
+    'confirmationText' => 'You will receive a confirmation email shortly. Please read carefully the information included in the confirmation email.<br>(You can close the present page.)',
+
+
+    'save' => 'REGISTRO',
+
+    'operations' => 'Operations',
+
+    'close' => 'Close',
+    'termsAndConditions' => 'Términos y Condiciones',
+    'agreeTermsAndConditions' => 'Acepto los <a data-toggle="modal" data-target="#termsAndConditions">términos y condiciones</a>'
+
+];
---
+++ resources/lang/es/platform.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Platform
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'title' => 'Registration platform',
+    'welcome' => 'Bienvenidos',
+    'register' => 'Registro',
+    'registered' => 'Registered',
+    'statistics' => 'Statistics',
+    'logout' => 'Cerrar sesión',
+];
